1166cf03ab54b9a6c954dd586ffc520eda9d88f9
amena kureshi <akureshi.laba@solvd.com>
2023-10-26 11:45:23 -0400

    'Project added'

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,26 @@
 // Programme of Insertion Sorting.
-// Programmer: Amena Kureshi
-// date: Oct 25, 2023
+//Programmer: Amena Kureshi
+//Date: Oct 26, 2023
+
 import java.util.Arrays;
-public class  Main {
-    
-    // Method to sort array by inserting element
+public class Main {
+
+    // Method to sort array
     public static void sorting(int[] array) {
         int n = array.length;
         for (int j = 1; j < n; j++) {
-            int k = array[j];
-            int i = j-1;
-            
-            while ( i >=0 && array [i] > k  ) {
-                array [i+1] = array [i];
+            int key = array[j];
+            int i = j - 1;
+            while (i >= 0 && array[i] > key) {
+                array[i + 1] = array[i];
                 i--;
             }
-            array[i+1] = k;
+            array[i + 1] = key;
         }
     }
-    
+
     public static void main(String[] args) {
-        int[] arr1 = {9,14,3,2,43,11,58,22};
+        int[] arr1 = {9, 14, 3, 2, 43, 11, 58, 22};
         System.out.println("Original Array");
         System.out.println(Arrays.toString(arr1));
             sorting(arr1);
